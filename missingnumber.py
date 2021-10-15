60a67ca2d6d284039bd047f36211cf81ea0c1f7b
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-15 14:25:55 +0530

    Algo for missing number program

--- missingnumber.py
+++ missingnumber.py
@@ -1,5 +1,10 @@
 #Program to find the missing number in a given list of n series
 #Find the missing number
+# Algo--
+# The sum of first n number is first found and then the given list's sum is found. 
+# Then finally we will get the missing number. Note that in every iteration the loop
+# should start from 1 rather 0
+
 num=10
 list1=[1,9,3,5,6,7,8,4,10]
 sum1=0
--- rotatearray.py
+++ rotatearray.py
@@ -2,7 +2,6 @@
 # the remaining elements that is from first to n-k-1 is reversed, Finally 
 # as a whole array/list is reversed, so that we get the array/list rotated
 
-
 a=[1,2,3,4,5]
 n=5
 k=2
@@ -10,7 +9,6 @@ k=2
 reverse(a,n-1,n-k)
 reverse(a,n-k-1,0)
 reverse(a,0,n-1)
-
 
 def reverse(a,l,h):
     pass
