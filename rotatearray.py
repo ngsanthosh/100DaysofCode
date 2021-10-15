2c66a93253f537a4c3ce4445bf5fcc69fe45a369
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-15 10:54:11 +0530

    Algorithm stated

--- rotatearray.py
+++ rotatearray.py
@@ -1,3 +1,8 @@
+# first the array/list from back till k elements is reversed and then 
+# the remaining elements that is from first to n-k-1 is reversed, Finally 
+# as a whole array/list is reversed, so that we get the array/list rotated
+
+
 a=[1,2,3,4,5]
 n=5
 k=2
@@ -8,4 +13,4 @@ reverse(a,0,n-1)
 
 
 def reverse(a,l,h):
-    
+    pass
