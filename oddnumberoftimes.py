74b01ef2db4e9935ab2ed0e082c03b8f6b1e4833
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-16 07:48:57 +0530

    Optimal solution of odd number of times finding problem

---
+++ oddnumberoftimes.py
@@ -0,0 +1,12 @@
+#Python program to find the number that is occuring odd number of times in a given list
+# Algo:
+# We use XOR operator, that if values are same then we get 0, if values are different we get 1
+# so if even number of times we get zero and if odd number of times we get one. 
+# Time complexity if O(n) and Space complexity is O(1)
+a=[1,2,2,4,5,4,4]
+fin=0
+
+for i in a:
+    fin = fin ^ i
+
+print(fin)
