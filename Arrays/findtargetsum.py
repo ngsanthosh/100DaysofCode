ec5bb315c898fa1474ba155aa6b7b518377886a7
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-16 08:51:34 +0530

    TwoSum

---
+++ Arrays/findtargetsum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        gonna=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                # if(nums[i]==nums[j]):
+                #     pass
+                # else:s
+                    if(nums[i]+nums[j]==target):
+                        return [i,j]
+                    else:
+                        continue
