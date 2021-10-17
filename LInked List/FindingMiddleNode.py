08458c45adc09cb1dd60530f309b4ba1b7c6fa22
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-17 10:09:46 +0530

    Algo and desc. for middleofLL

--- LInked List/FindingMiddleNode.py
+++ LInked List/FindingMiddleNode.py
@@ -1,3 +1,12 @@
+# Python Program to find the middle node of the linked list.
+
+'''Algo:
+We first have two pointers pointing on head node, and the pointer double goes two steps ++ 
+at a time and the pointer
+single does one step ++ at a time. So this keeps on happening till the double pointer is null 
+or next of double pointer is null. Finally the single pointing node will be the middle element 
+obviously'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
