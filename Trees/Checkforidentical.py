d30a0180e9c29d8ab779dd280fc318938e245c12
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-18 09:57:10 +0530

    Level order Traversal of Tree

--- Trees/Checkforidentical.py
+++ Trees/Checkforidentical.py
@@ -42,3 +42,23 @@ if __name__=='__main__':
         print("Trees are identical")
     else:
         print("Trees are not identical")
+
+
+
+
+#Leetcode
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if(p is None and q is None):
+            return True
+        elif p is not None and q is not None:
+            return p.val==q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+'''
