55a4c4aa1ea2e8e8219305f67e728538cbd85c1b
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-27 18:16:16 +0530

    MinHeap to MaxHeap Conversion

---
+++ Heap/MinHeaptoMaxHeap.py
@@ -0,0 +1,10 @@
+# The task is to convert a min heap to max heap
+# The tricky part is that they are confusing us giving
+# that we have min heap and convert it to max heap. but
+# given an unsorted array, we can build a max heap in 
+# O(n) time
+
+from heapq import heapify
+
+def mintomax(arrayy):
+    return heapify(array)
