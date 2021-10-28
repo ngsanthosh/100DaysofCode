9c658249984842453a1186c786050ec7fbb18aab
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-10-28 22:02:07 +0530

    Recursive approach of counting leaf nodes

---
+++ Trees/CountLeafNodes.py
@@ -0,0 +1,35 @@
+# Given the root of a binary tree, count the number of leap nodes present.
+#Recursive Approach
+
+import sys
+class node: 
+
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None
+        self.right = None 
+    
+def insert(ptr,key):
+    if(ptr is None):
+        ptr=node(key)
+    elif(key<=ptr.info):
+        ptr.left=insert(ptr.left,key)
+    elif(key>ptr.info):
+        ptr.right=insert(ptr.right,key)
+    return ptr
+
+def getLeafcount(root):
+    if(root==None):
+        return 0
+    if(root.left== None and root.right==None):
+        return 1
+    
+    return getLeafcount(root.left)+getLeafcount(root.right)
+
+if __name__=='__main__':
+    root=None
+    root=insert(root,10)
+    root=insert(root,5)
+    root=insert(root,15)
+    root=insert(root,30)
+    print(getLeafcount(root))
