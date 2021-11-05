3e23e63c2d4fd9e77bffd8149b167625b9370648
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-05 09:49:22 +0530

    Leaders - Algo

--- Arrays/FindLeaders.py
+++ Arrays/FindLeaders.py
@@ -1,7 +1,11 @@
+# The idea behind this prob is that, we traverse the array from back side, and maintain a max size, that is by 
+# comparing each and every element with max size. initially max size is - max size. 
+# Time O(n) and space O(1)
+
 import sys
 def findLeaders(arr,n):
     max_value=-sys.maxsize -1
-    for i in range(n-1,-1,-1):
+    for i in range(n-1,-1,-1): #Alter: for i in reversed(range(0,n)):
         if(max_value<arr[i]):
             max_value=arr[i]
             print(max_value)
