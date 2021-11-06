f4a967589bc45d681dde6a6c282dc3932b0d63bc
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-06 21:15:33 +0530

    Rotten Oranges

---
+++ Graphs/RottenOranges.py
@@ -0,0 +1,44 @@
+# CODE NOT YET FINALIZED, TEST CASES ARE FAILING
+
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        freshoranges=0
+        queue = deque()
+        for r in range(row):
+            for c in range(col):
+                if(grid[r][c]==2):
+                    queue.append((r,c))
+                elif(grid[r][c]==1):
+                    freshoranges+=1
+        directions=[(-1,0),(0,-1),(1,0),(0,1)]
+        timetook=-1
+        queue.append((-1,-1))
+        while queue:
+            row, col = queue.popleft()
+            if(row==-1):
+                timetook+=1
+                if queue:
+                  queue.append((-1,-1))  #Checkpoint
+            else:
+                for d in directions:
+                    n_row, n_col = row + d[0], col + d[1]
+                    if row>n_row>=0 and col>n_col>=0:
+                        if grid[n_row][n_col]==1:
+                            grid[n_row][n_col]=2
+                            freshoranges-=1
+                            queue.append((n_row, n_col))
+        return timetook if freshoranges==0 else -1 
+                            
+                            
+                            
+                        
+            
+                
+            
+            
+                
+        
+        
