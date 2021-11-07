774bcbf2593b02b483ed1f19c435b1b1fd4ecc91
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-07 10:22:12 +0530

    Parenthesis Matching

---
+++ Stack/ParenthesisMatching.py
@@ -0,0 +1,89 @@
+#Stack implemented using LL
+import sys
+class node: 
+
+    def __init__(self, info): 
+        self.info = info  
+        self.next = None 
+
+
+class Stack: 
+
+    def __init__(self): 
+        self.top = None
+        
+    
+    def isEmpty(self):
+        if self.top is None:
+            return True
+        return False
+    
+    def push(self,data):
+        self.temp=node(data)
+        if self.temp is None:
+            print("Stack overflow")
+            return
+        self.temp.next=self.top
+        self.top=self.temp
+    
+    def pop(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+            sys.exit(0)
+        d=self.top.info
+        self.top=self.top.next    
+        return d
+    
+    def peek(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+            sys.exit(0)
+        d=self.top.info
+        return d
+    def display(self):
+        if self.isEmpty():
+            print("Stack Underflow")
+            sys.exit(0)
+        self.p=self.top
+        while self.p is not None:
+            print(self.p.info)
+            self.p=self.p.next
+            
+def match(a,b):
+    if(a=='[' and b==']'):
+        return 1
+    if(a=='{' and b=='}'):
+        return 1
+    if(a=='(' and b==')'):
+        return 1
+    return 0
+def checkParantheses(s):
+    stack=Stack()
+    for i in range(len(s)):
+        if s[i]=='(' or s[i]=='[' or s[i]=='{':
+            stack.push(s[i])
+        if s[i]==')' or s[i]==']' or s[i]=='}':
+            if stack.isEmpty():
+                print("Right parentheses are more than left parentheses")
+                return 0
+            else:
+                temp=stack.pop()
+                if match(temp,s[i]) != 1:
+                    print("Mismatched parentheses are : {} {}".format(temp,s[i]))
+                    return 0
+        
+    if stack.isEmpty() is True:
+        print("Balanced Parentheses")
+        return 1
+    else:
+        print("Left parentheses more than right parentheses")
+        return 0
+
+            
+if __name__=='__main__':
+    s="((a+b)+(c+d))"
+    valid=checkParantheses(s)
+    if valid==1:
+        print("Valid Expression")
+    else:
+        print("Invalid Expression")
