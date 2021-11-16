a4843f8da459bb313e6e1892daa4033d7e83fbd9
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-16 19:55:55 +0530

    Decode ways

---
+++ Strings/DecodeWays.py
@@ -0,0 +1,19 @@
+#We use Dynamic Programming here...we take each index value of string one by one and solve for it.
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def helper(i):
+            if i in dic:
+                return dic[i]
+            if i >= len(s):
+                return 1
+            res = 0
+            if 1 <= int(s[i]) <= 9:
+                res += helper(i+1)
+            if 10 <= int(s[i:i+2]) <= 26:
+                res += helper(i+2)
+            dic[i] = res
+            return res
+        dic = {}
+        return helper(0) if s else 0
