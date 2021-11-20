0e7ae952cf9cad5439fd20ad176f5398f9c94483
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-20 12:14:55 +0530

    N largest Element in a list

---
+++ Heap/Klargest.py
@@ -0,0 +1,9 @@
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashit=Counter(nums)
+        return heapq.nlargest(k, hashit.keys(),key= hashit.get)
+        print(hashit.get)
