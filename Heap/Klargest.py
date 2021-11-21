5d1f8af10be5d20e1f39f0ae402eb284cdbd299d
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-21 14:42:28 +0530

    Algo for K largest

--- Heap/Klargest.py
+++ Heap/Klargest.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/top-k-frequent-elements/
 
+# ALgo: First we construct a hashmap which has the counter for all variables, then we construct a max heap 
+# and in that we sort the hashtmap with key having it key as the number of times it appeared!! then we just return it
+
 from collections import Counter
 import heapq
 
