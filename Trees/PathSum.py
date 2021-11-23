bc770e3c8041ace7fd479e06083a44965475cdf7
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-11-23 18:49:50 +0530

    Algo for path sum

--- Trees/PathSum.py
+++ Trees/PathSum.py
@@ -1,4 +1,9 @@
 # https://leetcode.com/problems/path-sum/
+# Algo:
+# The root of the tree is given and we need to traverse the path in which sum is equal to given target sum,
+# so we check if given root is none and we keep on subracting the value of current node from the total target sum and 
+# if that node has no left and right node, also if our sum counter is zero, then we return that True
+
 
 # Definition for a binary tree node.
 # class TreeNode:
