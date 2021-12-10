ef9d4506c383d1d459ac707df44202a2c07c895a
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-12-10 23:05:12 +0530

    Algo for Staircase

--- Strings/Staircase.py
+++ Strings/Staircase.py
@@ -1,3 +1,6 @@
+# Print the staircase pattern by just running an for loop also the for loop i in range from 1 to last index + 1 
+# Then we print spaces for n-i and # for i times for the current iteration of 'i' that matter.
+
 # https://www.hackerrank.com/challenges/staircase/problem
 
 def staircase(n):
