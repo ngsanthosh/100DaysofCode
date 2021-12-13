4aa998974d1d2f1341530aec385dc615b1e6d1a5
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-12-13 22:00:52 +0530

    algo for power of 2

--- Strings/isPowof2.py
+++ Strings/isPowof2.py
@@ -1,3 +1,7 @@
+The logic of the program is that, we take log base 2 of the given value and we check ceil
+and floor value of them..if both are same then...we can know that its an whole number is thus a 
+power of 2 definetely
+
 import math
 a=int(input("Enter the number"))
 
