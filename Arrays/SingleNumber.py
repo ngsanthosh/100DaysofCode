fcddf07a7a4b800b18fa118088886b6b4e7af7b7
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-12-15 19:13:06 +0530

    Single number algo

--- Arrays/SingleNumber.py
+++ Arrays/SingleNumber.py
@@ -1,6 +1,10 @@
 # Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
 
+# Algo: We are just finding the xor of it, like xor of anything that arrives only once in 
+# an array is 1 that is, if 0 xor 1 is 1 while 1 xor something else(other then 1) is not 1, so 
+# we return the result
+
 def getOddOccurence(arr):
     res=0
     for i in arr:
