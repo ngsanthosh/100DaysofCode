0225bb51b62691629e6a2c9044cd4c2608fc5516
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-12-16 22:16:17 +0530

    Algo for Maximum Depth

--- Trees/MaximumDepth.py
+++ Trees/MaximumDepth.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/maximum-depth-of-binary-tree/
 
+# Algo: we look for the maximum depth of the tree with using recursion with finding 1 + left node recursively and 
+# right node recursively. Finally we return them as a result, in this way if no root is found we return 0
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
