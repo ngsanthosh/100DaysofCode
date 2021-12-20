f4e4851d4ff9d64b2708ec682ce8105d9f1aef78
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2021-12-20 11:50:19 +0530

    Validate BST

---
+++ Trees/ValidateBinTree.py
@@ -0,0 +1,31 @@
+import sys
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        if not root:
+            return True
+        lower = -sys.maxsize
+        upper = sys.maxsize
+             
+        stack = [(root, lower,upper)]
+        
+        while stack:
+            
+            root, lower, upper = stack.pop()
+            
+            if(not root):
+                continue
+            val = root.val
+            if(val<=lower or val>=upper):
+                return False
+            else:
+                stack.append((root.left, lower, val))
+                stack.append((root.right, val, upper))
+            
+        return True
