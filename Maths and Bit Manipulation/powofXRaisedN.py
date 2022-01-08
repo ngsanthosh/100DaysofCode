7bcd6c204a38c00d5871105a6a96de8b44dce798
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-08 14:37:13 +0530

    Alter method and its algo

--- Maths and Bit Manipulation/powofXRaisedN.py
+++ Maths and Bit Manipulation/powofXRaisedN.py
@@ -1,5 +1,10 @@
 #https://leetcode.com/problems/powx-n/
 
+
+For Other approach without using built in function, refer the photo along with name capture.png
+
+# Algo: The algo of the capture file program is that, for e.g. instead of doing 2pow7 2 times 7 ,
+# we do ((2 times 3) * 2 + 1 time 2) .. We generally split and find the power resultt...
 import math
 
 class Solution:
