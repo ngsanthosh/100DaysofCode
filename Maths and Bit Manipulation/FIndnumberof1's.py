b65227960cd16a5dc69abe332e76961a7a54d09c
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-11 10:51:38 +0530

    Algo

--- Maths and Bit Manipulation/FIndnumberof1's.py
+++ Maths and Bit Manipulation/FIndnumberof1's.py
@@ -16,8 +16,8 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
         while n:
-            count += n & 1
-            n=n>>1
+            count += n & 1 #Binary ANDing
+            n=n>>1 #Right Shifting by 1
         return count
         
         
