f7b98f3c8dcca3f968549ee0982786091d46151b
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-11 09:36:10 +0530

    Optimal solution of finding the peak element

--- Searching and Sorting/FIndPeakElement.py
+++ Searching and Sorting/FIndPeakElement.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/find-peak-element/
 
+# O(log n) approach - https://extraimage.info/pix/image.RM1W8B
+
+#O(n) approach using built in function
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         a=sorted(nums)
