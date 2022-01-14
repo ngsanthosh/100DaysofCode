957ac8e9fe8e71087d5deddbf43fbc5a6a7622e0
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-14 14:58:22 +0530

    Divisible sum pair

---
+++ Arrays/DivisibleSumPair.py
@@ -0,0 +1,9 @@
+# O(n^2) approach
+
+def divisibleSumPairs(n, k, ar):
+    count=0
+    for i in range(len(ar)):
+        for j in range(i+1,len(ar)):
+            if((ar[i]+ar[j])%k==0):
+                count+=1
+    return count
