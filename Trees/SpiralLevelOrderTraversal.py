06d2206c6a2fa2071e9cc7c001c18a0d9e4b1cbc
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-17 21:51:55 +0530

    Spiral Order Traversal

---
+++ Trees/SpiralLevelOrderTraversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        stack1=[]
+        stack2=[]
+        result=[]
+        stack1.append(root)
+
+        while (len(stack1)>0) or (len(stack2)>0):
+            while(len(stack1)>0):
+                temp=stack1.pop()
+                result.append(temp.val)
+                if(temp.right is not None):
+                    stack2.append(temp.right)
+                if(temp.left is not None):
+                    stack2.append(temp.left)
+            while(len(stack2)>0):
+                temp=stack2.pop()
+                result.append(temp.val)
+                if(temp.left is not None):
+                    stack1.append(temp.left)
+                if(temp.right is not None):
+                    stack1.append(temp.right)
+                    
+        return result
+        
