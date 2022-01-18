272cb3eb21fd3414cea05fd35ceadd1cbeb5cede
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-18 22:08:42 +0530

    Reverse words in a string

---
+++ Strings/ReverseWordsinString.py
@@ -0,0 +1,15 @@
+
+a="I love you"
+
+b=list(a.split(' ')) #Spliting based on space and converting to list
+b.reverse() #Reverse a List
+# or else
+# c=b[::-1] #This also reverses a list
+# print(c)
+# print(b)
+# result="-"
+# for i in c:
+#     result+= i+" "
+bb=" ".join(b) #We can join a list to a string using "join" keyword
+print(bb)
+# print("result",result)
