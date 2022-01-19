02f692ee784423386c1fd2a6012f3206c0ee0787
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-19 12:33:37 +0530

    Metho2

--- Arrays/TransistionPoint.py
+++ Arrays/TransistionPoint.py
@@ -5,3 +5,8 @@ def transitionPoint(arr, n):
         return arr.index(1) 
     except:
         return -1
+    # Method2
+    for i in range(len(arr)):
+        if(arr[i]==1):
+            return i
+    return -1
