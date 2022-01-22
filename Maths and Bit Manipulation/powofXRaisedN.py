ccdc2fd9d9b18bb8114431a76261de8553606057
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-22 21:50:25 +0530

    Algo for reversing only words

--- Maths and Bit Manipulation/powofXRaisedN.py
+++ Maths and Bit Manipulation/powofXRaisedN.py
@@ -1,7 +1,7 @@
 #https://leetcode.com/problems/powx-n/
 
 
-For Other approach without using built in function, refer the photo along with name capture.png
+# For Other approach without using built in function, refer the photo along with name capture.png
 
 # Algo: The algo of the capture file program is that, for e.g. instead of doing 2pow7 2 times 7 ,
 # we do ((2 times 3) * 2 + 1 time 2) .. We generally split and find the power resultt...
--- Strings/ReverseWordsinString.py
+++ Strings/ReverseWordsinString.py
@@ -1,4 +1,7 @@
-# Given a sentence of words, reverse the words in that sentence (p.s. not letters)
+# Given a sentence of words, reverse the words in that sentence (p.s. not letters)'
+
+# Here we first split the string in terms of list and then we reverse the list and then again using 
+# join method, we join the list and return that as output
 
 a="I love you"
 
@@ -14,3 +17,5 @@ b.reverse() #Reverse a List
 bb=" ".join(b) #We can join a list to a string using "join" keyword
 print(bb)
 # print("result",result)
+
+#OUTPUT: you love I
