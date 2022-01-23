c2e5a2ded6e26cd7910aaee744df9530d4911f28
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-23 11:08:13 +0530

    Remove Duplicates in a list in place

---
+++ Arrays/RemoveDupsinplace.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        #Meth1
+        a=nums.count(val)
+
+        for i in range(a):
+            nums.remove(val)
+        
+        return len(nums)    
+        
+        #Meth2
+        while val in nums:
+            nums.remove(val)
+            
+        return len(nums)
+        
