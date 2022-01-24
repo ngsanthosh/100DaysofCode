588737e24926d99f04a3aca29c575f6d66eda380
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-24 20:20:46 +0530

    Optimum solution of grouping anagram problem

--- Strings/GroupAnagrams.py
+++ Strings/GroupAnagrams.py
@@ -8,3 +8,12 @@ for stu in strs:
     anag[tuple(sorted(stu))].append(stu)
 print(anag)
 print ([v for v in anag.values()])
+
+# T - O(n L) S- O(26*L)~O(1) where L is the length of each string
+anag= defaultdict(list)
+for stu in strs:
+    count = [0]*26
+    for i in stu:
+        count[ord(i)-ord('a')]+=1
+    anag[tuple(count)].append(stu)
+print (anag.values())
