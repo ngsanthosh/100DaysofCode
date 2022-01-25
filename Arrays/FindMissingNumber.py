d13a7daf0315e197592788eeefd57f6ae34522ad
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-25 08:43:00 +0530

    Missing Number

---
+++ Arrays/FindMissingNumber.py
@@ -0,0 +1,33 @@
+
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # a=[i for i in range(1,len(nums)+1)]
+        # print(a)
+        # # result=[]
+        # for i in nums:
+        #     if i in a:
+        #         a.remove(i)
+        # return a
+        
+        for n in nums:
+            i = abs(n) - 1
+            nums[i]=-1*abs(nums[i])
+        
+        res=[]
+        for i,v in enumerate(nums):
+            if(v>0):
+                res.append(i+1)
+        return res
+            
+            
+            
+            
+            
+            
+        
+        
+            
