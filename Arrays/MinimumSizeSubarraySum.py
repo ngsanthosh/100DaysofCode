f49f38366fb5ddd8a082684d92d97e78d26f3290
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-28 11:42:12 +0530

    Minimum size sub array sum

---
+++ Arrays/MinimumSizeSubarraySum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        low, total= 0,0
+        res=len(nums)+1
+        
+        for right in range(len(nums)):
+            total+=nums[right]
+            while total>=target:
+                res=min(right-low+1,res)
+                total-=nums[low]
+                low+=1
+            
+        return 0 if res==len(nums)+1 else res
+            
+        
