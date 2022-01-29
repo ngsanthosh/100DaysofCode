2a6f3bcb31fdb076d64c3cee0a6e7983a2b7a25d
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-29 11:39:59 +0530

    Isomorphic Strings

---
+++ Strings/IsomorphicStrings.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        ltr,rtl={},{}
+        
+        for i in range(len(t)):
+            c1=s[i]
+            c2=t[i]
+            
+            if((c1 in ltr and ltr[c1]!=c2) or (c2 in rtl and rtl[c2]!=c1)):
+                return False
+            
+            ltr[c1]=c2
+            rtl[c2]=c1
+            
+            
+        return True
+            
