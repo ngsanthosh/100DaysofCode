c470bdaeda5588873642140ca5b063fd0bacd318
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-01-30 11:18:14 +0530

    Linkuu and Algo

--- Dynamic Programming/Nimgame.py
+++ Dynamic Programming/Nimgame.py
@@ -1,3 +1,7 @@
+# https://leetcode.com/problems/nim-game/
+
+# Algo: Given n number of stones, in a turn only 1 to 3 stones can be taken by a player, and so if n=4 last stone will be taken
+# by opponent and i lose, so whenever there is a multiple of 4, i lose and opponent wins
 
 class Solution(object):
     def canWinNim(self, n):
