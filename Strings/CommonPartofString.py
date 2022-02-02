71727378b65834cf4906106aa9a11a1bb21b7e6a
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-02 14:13:45 +0530

    Common part between two strings

---
+++ Strings/CommonPartofString.py
@@ -0,0 +1,19 @@
+a="geeksforgeeks"
+b="practiceforgeeks"
+c=""
+# ans=[]
+prevlen=len(c)
+for i in b:
+    if(i in a):
+        # if(i not in c):
+            c+=i
+# if(len(c)>prevlen):
+#     ans.append(c)
+#
+# print(ans[-1])
+
+
+
+# c=a|b
+# print(c)
+print("".join(sorted(c)))
