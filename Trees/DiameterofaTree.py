e12a29698106b2307777b7b9f89c15ddd4e8132f
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-04 13:42:30 +0530

    Diameter of a Treee

---
+++ Trees/DiameterofaTree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        res=[0]
+        
+        def deefs(root):
+            if not root:
+                return -1
+            left = deefs(root.left)
+            right= deefs(root.right)
+            
+            res[0]=max(res[0],2+left+right)
+            
+            return 1+max(left,right)
+        
+        deefs(root)
+        
+        return res[0]
+        
+
+        
