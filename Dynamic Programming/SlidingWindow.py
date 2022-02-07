2650aaa6936c985fb74aaf48461420c8866b02cd
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-07 22:02:54 +0530

    Working for Sliding Window

--- Dynamic Programming/SlidingWindow.py
+++ Dynamic Programming/SlidingWindow.py
@@ -1,6 +1,14 @@
 # https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
 
+# Actually we traverse through all of the window and find the sum of each window and if it is equal to the sum
+# then we increment the count
+
+# #Optimal
+# the same above process but we dont find sum in every window, we just move the window a step and subract the left 
+# most number that was in the previous window
+
+
 
 #My brute force approach
 def birthday(s, d, m):
