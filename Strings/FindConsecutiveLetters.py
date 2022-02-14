4492e4a3688b835a3f4be0f6546b7334cc4be28e
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-14 11:04:06 +0530

    Linkuuu

--- Strings/FindConsecutiveLetters.py
+++ Strings/FindConsecutiveLetters.py
@@ -1,3 +1,6 @@
+# https://media-exp1.licdn.com/dms/image/C4E22AQEXuAZqBDGx5g/feedshare-shrink_2048_1536/0/1644808000534?e=1647475200&v=beta&t=kSnkCgGAC36H5fF5tbanFrAGQqpbCGgMCkf1BLqk_1c
+
+
 
 a="malayalam"
 find="a"
