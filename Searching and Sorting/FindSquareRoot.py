a4e69113f6f3796f6d649d4fd8044120f49cc705
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-16 10:41:06 +0530

    Finding Perfect Square

---
+++ Searching and Sorting/FindSquareRoot.py
@@ -0,0 +1,31 @@
+
+
+
+# O(sqrt(n)) for brute force approach
+# O(log n) solution using binary search intution
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # a=[x*x for x in range(num+1)]
+        # print(a)
+        # print(num)
+        # return True if (num in a) else False
+        
+        l, r = 1 , num
+        
+        while l<=r:
+            mid = (l+r) // 2
+            
+            if(mid*mid>num):
+                r=mid-1
+            elif(mid*mid<num):
+                l=mid+1
+            else:
+                return True
+        return False
+                
+            
+        
