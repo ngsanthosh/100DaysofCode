f9a15288e22b9f41b3f8dd29713f7f331066f591
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-16 19:27:33 +0530

    Word Pattern 1

---
+++ Strings/WordPattern.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        # if()
+        words=s.split(" ")
+        ctw={}
+        wtc={}
+        if(len(pattern)!=len(words)):
+            return False
+        
+        for c,w in zip(pattern, words):
+            if c in ctw and ctw[c]!=w:
+                return False
+            if w in wtc and wtc[w]!=c:
+                return False
+            ctw[c]=w
+            wtc[w]=c
+        return True
+        
+        
