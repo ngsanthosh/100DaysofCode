6edf5d2cdee81ce8c2c0f7a6cec9da2e8e4675db
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-17 21:02:52 +0530

    Hash Countries

---
+++ Strings/HashCountries.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number of test cases"))
+dic={}
+for i in range(n):
+    inp=input()
+    lst=inp.split(" ")
+    dic[lst[0]]=lst[1]
+
+search=input("Enter the search term")
+print(dic)
+print(dic[search])
