a7a922d37f9e61bbb8c8c89b62e657f713a25852
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-19 22:43:40 +0530

    Algo for consecutive letters.

--- Strings/FindConsecutiveLetters.py
+++ Strings/FindConsecutiveLetters.py
@@ -1,5 +1,7 @@
 # https://media-exp1.licdn.com/dms/image/C4E22AQEXuAZqBDGx5g/feedshare-shrink_2048_1536/0/1644808000534?e=1647475200&v=beta&t=kSnkCgGAC36H5fF5tbanFrAGQqpbCGgMCkf1BLqk_1c
 
+# Here we just add those two strings and then we find whether the resultant string is available or not in given string. 
+# If yes we return the count, else -1 
 
 a="malayalam"
 find="a"
