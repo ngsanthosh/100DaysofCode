a3b6900574f58170d0d4d4712b8fb8a55f3cdcde
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-19 23:03:28 +0530

    Strong Number

---
+++ Maths and Bit Manipulation/StrongNumber.py
@@ -0,0 +1,23 @@
+
+# n=int(input())
+
+def fact(n):
+    if(n==0):
+        return 1
+    
+    return (n * fact(n-1))
+            
+    
+    
+# print(fact(n))
+
+t=int(input("Enter the strong or not number"))
+check=t
+sum=0
+while t!=0:
+    getit=t%10
+    sum+=fact(getit)
+    t=t//10
+# print(t)
+# print(sum)
+print(check==sum)
