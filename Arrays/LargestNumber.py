5323e21451ddb82fd109337003afd9179f17d680
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-26 19:33:05 +0530

    Largest Number

---
+++ Arrays/LargestNumber.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        # i=nums[0]
+        # j=nums[1]
+        
+        numss=[str(h) for h in nums]
+        # print(numss)
+        for i in range(len(numss)):
+            for j in range(i+1, len(numss)):
+                # print(numss[i]+numss[j],numss[j]+numss[i])
+                if(numss[i]+numss[j] > numss[j]+numss[i]):
+                    continue
+                else:
+                    numss[i],numss[j]=numss[j],numss[i]
+        return str(int("".join(numss)))
