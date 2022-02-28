cc4477055342381850ef16d67617e0396375eab0
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-02-28 20:48:00 +0530

    Find the date

---
+++ Maths and Bit Manipulation/FindDayofDate.py
@@ -0,0 +1,14 @@
+a="TUE"
+b=19
+day=["MON","TUE","WED","THU","FRI","SAT","SUN"]
+# find=b%8
+# # print(day.index(a))
+# print(day[((day.index(a)+1)+find)-1])
+i=day.index(a)
+
+check=b%7
+
+final=i+check-1
+# print(final)
+
+print(day[final%7])
