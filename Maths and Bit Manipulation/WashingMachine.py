b22ad73c523d850649716f7a3c94675645345425
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-05 12:17:23 +0530

    Washing Machine

---
+++ Maths and Bit Manipulation/WashingMachine.py
@@ -0,0 +1,16 @@
+
+
+a=int(input("Enter the kg"))
+
+if(1<=a<=2000):
+    print("25")
+elif(2001<=a<=4000):
+    print("35")
+elif(4001<=a<=7000):
+    print("35")
+elif(a>7000):
+    print("overloaded")
+elif(a==0):
+    print("0")
+else:
+    print("Invalid Output")
