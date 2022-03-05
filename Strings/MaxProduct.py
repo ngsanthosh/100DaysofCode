eaf1bcb2131941f5dbdfcdfdf54517ca474908b1
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-05 12:46:47 +0530

    Maximum Product of Word Lengths

---
+++ Strings/MaxProduct.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        
+        def nocommon(str1,str2):
+            for i in str1:
+                if(i in str2):
+                    return False
+            return True
+        maxx=0
+        for x in range(len(words)):
+            for y in range(x+1,len(words)):
+                if(nocommon(words[x],words[y])):
+                    maxx=max(maxx,len(words[x])*len(words[y]))
+                    
+                    
+        return maxx
+                
+            
