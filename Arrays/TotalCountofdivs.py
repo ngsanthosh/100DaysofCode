eb72b28efa774f3ca96ee7dfcea254cae670495b
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-07 14:10:22 +0530

    Total Count

---
+++ Arrays/TotalCountofdivs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def totalCount(self, arr, n, k):
+        # code here
+        count=0
+        for i in arr:
+            if(i%k!=0):
+                d=(i//k)+1
+            else:
+                d=(i//k)
+            count+=d
+        return count
