add5cb45b7fef57a4e86935e0c5db120994b4839
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-07 14:13:15 +0530

    Correcto

--- Arrays/MaximumProductof2Nums.py
+++ Arrays/MaximumProductof2Nums.py
@@ -1,14 +1,13 @@
 class Solution:
-
 	def maxProduct(self,arr, n):
 		# code here
         # O(n logn) solution
         #a=sorted(arr)
         #return a[-1]*a[-2]
 
-        #O(n) solution
-        max1=max(arr)
-        arr.remove(max1)
-        max2=max(arr)
-        
-        return max1*max2
+            #O(n) solution
+            max1=max(arr)
+            arr.remove(max1)
+            max2=max(arr)
+            
+            return max1*max2
