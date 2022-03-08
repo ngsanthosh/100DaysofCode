9a5f5ed1db2223ed06ed74f344c5f5639c7cbad7
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-08 22:25:20 +0530

    replace 0s with 5s

---
+++ Maths and Bit Manipulation/Replace0with5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def convertFive(self,n):
+        #Code here
+        # nn=list(str(n))
+        # # print(nn)
+        # for i in range(len(nn)):
+        #     if(nn[i]=='0'):
+        #         nn[i]='5'
+                
+        # # print(nn)
+        # return int("".join(nn))
+        
+        copy=n
+        power=1
+        while(n>0):
+            digit=n%10
+            if(digit==0):
+                copy+=power*5
+            power*=10
+            n//=10
+        return copy
