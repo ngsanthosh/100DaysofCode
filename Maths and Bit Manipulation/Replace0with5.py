176c3ba162d39afe415bb67c0421f2cf386d8ba6
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-09 07:17:27 +0530

    Time complexities

--- Maths and Bit Manipulation/Replace0with5.py
+++ Maths and Bit Manipulation/Replace0with5.py
@@ -1,6 +1,8 @@
 class Solution:
     def convertFive(self,n):
         #Code here
+
+        #O(n) solution
         # nn=list(str(n))
         # # print(nn)
         # for i in range(len(nn)):
@@ -10,6 +12,7 @@ class Solution:
         # # print(nn)
         # return int("".join(nn))
         
+        ##O(n) solution - But Optimal Approach
         copy=n
         power=1
         while(n>0):
