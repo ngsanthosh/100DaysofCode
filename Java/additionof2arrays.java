3b645194d8c722d13c147ea56d75768d03b8c6e2
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-09 18:51:38 +0530

    Change

--- Java/additionof2arrays.java
+++ Java/additionof2arrays.java
@@ -5,7 +5,8 @@ public class Main
 		int[] a={1,2,3,4,5};
 		int[] b={6,7,8,9,10};
 		
-		int[] c={0,0,0,0,0};
+		int[] c={0,0,0,0,0}; 
+        // int[] c = new int[a.length]; -- Alternate if size is to be fixed
 		for(int i=0;i<a.length;i++){
 		    c[i]=a[i]+b[i];
 		}
