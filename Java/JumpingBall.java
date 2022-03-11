2db402a93da0359437562089bb0fc8bccedc797e
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-11 06:52:01 +0530

    jumping ball

---
+++ Java/JumpingBall.java
@@ -0,0 +1,11 @@
+class Solution{
+    static long jumpyBall(long N){
+        // code here
+        long add=0;
+        while(N!=0){
+            add=add+N;
+            N=N/2;
+        }
+        return add*2;
+    }
+}
