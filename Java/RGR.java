fab87595ba44fe597c5fa315f917c5240af6e814
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-11 06:27:35 +0530

    Find Minimum changes for same colour

---
+++ Java/RGR.java
@@ -0,0 +1,30 @@
+class Solution {
+    static int RedOrGreen(int N, String S) {
+        //code here
+        int mini=Integer.MAX_VALUE;
+        HashMap<Character, Integer> maps= new HashMap<Character, Integer>();
+        
+        for(int i=0;i<N;i++){
+            if(maps.containsKey(S.charAt(i))){
+                int old = maps.get(S.charAt(i));
+                maps.put(S.charAt(i),old+1);
+            }
+            else{
+                maps.put(S.charAt(i),1);
+            }
+        }
+        // System.out.println(maps.toString());
+        // return 0;
+        // int arr=maps.keySet();
+        for(int i: maps.values()){
+            if(i<mini){
+                mini=i;
+            }
+        }
+        
+       if(mini==N){
+           return 0;
+       }
+       return mini;
+    }
+}
