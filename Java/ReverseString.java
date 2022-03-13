337adb3a9ab6a8b0eee56682d580b579fed336e4
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-13 16:01:50 +0530

    REverse String

---
+++ Java/ReverseString.java
@@ -0,0 +1,27 @@
+class Solution
+{
+    public Boolean check(int x, String st){
+        for(int i=0;i<st.length();i++){
+            if(st.charAt(i)==x){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+    public String reverseString(String s)
+    {
+        String res="";
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) ==' '){
+                continue;
+            }
+            else{
+                if(check(s.charAt(i),res)){
+                    res+=s.charAt(i);
+                }
+            }
+        }
+        return res;
+    }
+}
