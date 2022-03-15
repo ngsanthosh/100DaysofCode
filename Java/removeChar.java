7694e1d094d4a7d4b1ae81ed21060de0cad14090
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-15 21:44:04 +0530

    Remove Char

---
+++ Java/removeChar.java
@@ -0,0 +1,20 @@
+https://practice.geeksforgeeks.org/problems/remove-character3815/0/#
+
+class Solution{
+    static String removeChars(String string1, String string2){
+        // code here
+        String out="";
+        HashMap <Character,Integer> yes= new HashMap<>();
+        for(int i=0;i<string2.length();i++){
+            yes.put(string2.charAt(i),1);
+        }
+        
+        for(int i=0;i<string1.length();i++){
+            if(yes.containsKey(string1.charAt(i))){
+                continue;
+            }
+            out+=string1.charAt(i);
+        }
+        return out;
+    }
+}
