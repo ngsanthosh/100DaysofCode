c77983f17c639e440109dc27e4173d58df64f732
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-17 07:17:54 +0530

    plus minus numbers

---
+++ Java/PlusMinus.java
@@ -0,0 +1,29 @@
+class Result {
+
+    public static void plusMinus(List<Integer> arr) {
+    // Write your code here
+    Double pos=0.0;
+    Double neg=0.0;
+    Double zero=0.0;
+    
+    for(int i=0;i<arr.size();i++){
+        if(arr.get(i)>0){
+            pos++;
+        }
+        else if(arr.get(i)<0){
+            neg++;
+        }
+        else{
+            zero++;
+        }
+    }
+    double posra = pos/arr.size();
+    double negra = neg/arr.size();
+    double zerora = zero/arr.size();
+    
+    System.out.printf("%.6f",posra);
+    System.out.printf("\n%.6f",negra);
+    System.out.printf("\n%.6f",zerora);
+    }
+
+}
