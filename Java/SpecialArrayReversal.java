746381748a9b7e3051bb33d72dd62bf7ec51afaa
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-19 06:56:13 +0530

    Special Array reversal

---
+++ Java/SpecialArrayReversal.java
@@ -0,0 +1,31 @@
+class Solution
+{
+    public String reverse(String s)
+    {
+        //complete the function here
+         //complete the function here
+       char[] str=s.toCharArray();
+       char a;
+       for(int i=0,j=s.length()-1;i<s.length() && i<=j;)
+       {
+       if(!Character.isAlphabetic(str[i]))
+       {
+       i++;    
+       }
+       else if(!Character.isAlphabetic(str[j]))
+       {
+       j--;    
+       }
+       else
+       {
+       a=str[i];
+       str[i]=str[j];
+       str[j]=a;
+       i++;
+       j--;
+       }
+       }
+//   String ans=String.valueOf(str);    
+    return String.valueOf(str);
+    }
+}
