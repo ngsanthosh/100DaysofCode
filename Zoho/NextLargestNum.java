59dca450d07f62965d8a2559b09e3f7c68d6cbd6
Santhosh Sudhaan N G <63006841+ngsanthosh@users.noreply.github.com>
2022-03-26 15:52:36 +0530

    Sorting String Numbers

--- Zoho/NextLargestNum.java
+++ Zoho/NextLargestNum.java
@@ -2,27 +2,19 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		int[] a = {3,54,62,2,12,59};
-        ArrayList<Integer> res = new ArrayList<>();
-        int n = a.length;
-        int temp;
-        for(int i=0;i<n;i++){
-          temp = -1; 
-          for(int j=i+1;j<n;j++){
-            if(temp==-1){
-                if(a[j]>a[i]){
-                  temp=a[j];
-                }
-            }
-            else{
-                if(a[j]<temp && a[j]>a[i]){
-                  temp=a[j];
-                }
-            }
-          }
-          res.add(temp);
-        }
-        System.out.println(res);
-
+		int a = 5
+		for(int i = 1 ; i <= a ; i++) {
+			for(int j = a ; j >= 1 ; j--) {
+				if(j <= i) {
+					System.out.print(j + " ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
 	}
+
+	
 }
---
+++ Zoho/SortNumbers.py
@@ -0,0 +1,39 @@
+# a=["Raja","Bhuvanu","Babu","Geetha","Karthi","Babiu"]
+def compare(str1,str2):
+    # print("Comparing",str1,str2)
+    i=0
+    j=0
+    while(i<len(str1) and j<len(str2)):
+        if(len(str1)==len(str2)):
+            if(str1[i]==str2[j]):
+                i+=1
+                j+=1
+            elif(str1[i]<str2[j]):
+                return False
+                break
+            else:
+                return True
+                break
+        else:
+            if(len(str1)>len(str2)):
+                return True
+            else:
+                return False
+
+        # return True
+def sorting(a):
+    count=0
+    for i in range(len(a)-1):
+        if(compare(a[i],a[i+1])):
+            count+=1
+            # print("Swapped")
+            a[i],a[i+1]=a[i+1],a[i]
+
+
+    return a if count==0 else sorting(a)
+
+# new=sorting(["Bhuvana","zoho","kaveri","Karthi","Babu","kavitha"])
+new=sorting(['8','15','14','1','2'])
+sorting(new)
+
+print(new)
